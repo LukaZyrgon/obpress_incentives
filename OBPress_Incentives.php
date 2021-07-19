278bcb357f74d965c3d208e80c68a9ed2a0c78ce
LukaZyrgon <58442639+LukaZyrgon@users.noreply.github.com>
2021-07-19 12:30:53 +0200

    Update OBPress_Incentives.php

--- OBPress_Incentives.php
+++ OBPress_Incentives.php
@@ -4,7 +4,7 @@
   Plugin uri: www.zyrgon.net
   Text Domain: OBPress_Incentives
   Description: Widgets to OBPress
-  Version: 0.0.1
+  Version: 0.0.2
   Author: Zyrgon
   Author uri: www.zyrgon.net
   License: GPlv2 or Later
@@ -19,12 +19,12 @@ if(get_option('obpress_api_set') == true){
 
 // TODO, MAKE GIT BRANCH, CONNECT WITH UPDATE CHECKER
 
-// require_once(WP_PLUGIN_DIR . '/OBPress_SearchBarPlugin/plugin-update-checker-4.11/plugin-update-checker.php');
-// $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-//     'https://github.com/MilosZyrgon/OBPress_SearchBarPlugin',
-//     __FILE__,
-//     'OBPress_SearchBarPlugin'
-// );
+require_once(WP_PLUGIN_DIR . '/OBPress_SearchBarPlugin/plugin-update-checker-4.11/plugin-update-checker.php');
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/LukaZyrgon/obpress_incentives',
+    __FILE__,
+    'OBPress_Incentives'
+);
 
-// //Set the branch that contains the stable release.
-// $myUpdateChecker->setBranch('main');}
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');}
